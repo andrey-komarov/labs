2c341a04b4f295e245669cf0bc2d74e254cc71af
Andrey Komarov <taukus@gmail.com>
2012-12-13 01:34:54 +0400

    Убогий эмулятор машины тьюринга

---
+++ 3/turing/src/Main.java
@@ -0,0 +1,12 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        File f = new File("input.txt");
+        TuringMachine tm = new TuringMachine(f);
+        System.err.println(tm);
+        System.err.println(tm.run(new String[] {"1", "1", "1", "1", "+", "1", "1"}, 1000000));
+    }
+
+}
